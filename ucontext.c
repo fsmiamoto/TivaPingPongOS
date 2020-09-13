926c255ebc2a23859490e78b1af2d7719115d1c8
Francisco Miamoto <fsmiamoto@gmail.com>
2020-09-13 14:21:24 -0300

    clean up ucontext files

--- ucontext.c
+++ ucontext.c
@@ -3,14 +3,9 @@
 extern int flag;
 extern int memPC;
 extern set_context_asm(ucontext_t*);
-extern get_context_asm (ucontext_t*);
-//int r;
-//int r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
+extern get_context_asm(ucontext_t*);
 
-//int t1 = 4;
-
-
-void makecontext(ucontext_t *context, int start_routine, int n_parms, char* parm)
-{
-context->func = start_routine;
+void makecontext(ucontext_t* context, int start_routine, int n_parms,
+                 char* parm) {
+  context->func = start_routine;
 }

ee41a6eb7ff2488aa4d5de0c3ba9111c9f33a07c
Francisco Miamoto <fsmiamoto@gmail.com>
2020-10-20 20:03:07 -0300

    feat: update main with P5 test

--- main.c
+++ main.c
@@ -4,43 +4,41 @@
 #include "ppos.h"
 #include "tiva_core.h"
 
+#define WORKLOAD 4000
+
 task_t Pang, Peng, Ping, Pong, Pung;
 
+// Simula um processamento pesado
+int hardwork(int n) {
+  int i, j, soma;
+
+  soma = 0;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++) soma += j;
+  return (soma);
+}
+
+// corpo das threads
 void Body(void *arg) {
-  int i;
-
-  UARTprintf("%s: inicio (prioridade %d)\n", (char *)arg, task_getprio(NULL));
-
-  for (i = 0; i < 10; i++) {
-    UARTprintf("%s: %d\n", (char *)arg, i);
-    task_yield();
-  }
-  UARTprintf("%s: fim\n", (char *)arg);
+  UARTprintf("%s: inicio em %4d ms\n", (char *)arg, systime());
+  hardwork(WORKLOAD);
+  UARTprintf("%s: fim    em %4d ms\n", (char *)arg, systime());
   task_exit(0);
 }
 
-int main(void) {
+int main() {
   TivaInit();
-
   UARTprintf("PingPongOS\n");
+
   UARTprintf("main: inicio\n");
 
   ppos_init();
 
   task_create(&Pang, Body, "    Pang");
-  task_setprio(&Pang, 0);
-
   task_create(&Peng, Body, "        Peng");
-  task_setprio(&Peng, 2);
-
   task_create(&Ping, Body, "            Ping");
-  task_setprio(&Ping, 4);
-
   task_create(&Pong, Body, "                Pong");
-  task_setprio(&Pong, 6);
-
   task_create(&Pung, Body, "                    Pung");
-  task_setprio(&Pung, 8);
 
   task_yield();
 
